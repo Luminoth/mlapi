21ad7934dc2b8b84c6abae6bcde916ee4fff65ed
Shane <ignignokterr@gmail.com>
2021-02-06 12:34:40 -0800

    Project setup checkpoint

---
+++ Assets/Scripts/mlapi/Loading/LoadingManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+using pdxpartyparrot.Game.Loading;
+
+namespace pdxpartyparrot.mlapi.Loading
+{
+    public sealed class LoadingManager : LoadingManager<LoadingManager>
+    {
+    }
+}
