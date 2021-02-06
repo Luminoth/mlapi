b7e3175589a6da9436bd629fcf4a835a37e2832c
Shane <ignignokterr@gmail.com>
2021-02-06 11:09:57 -0800

    Project setup checkpoint

---
+++ Assets/Scripts/Core/World/Waypoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace pdxpartyparrot.Core.World
+{
+    public class Waypoint : MonoBehaviour
+    {
+        [SerializeField]
+        private Waypoint _nextWaypoint;
+
+        public Waypoint NextWaypoint => _nextWaypoint;
+
+        #region Unity Lifecycle
+
+        protected virtual void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position, 1);
+        }
+
+        #endregion
+    }
+}
